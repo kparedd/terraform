57f8e1c48f0e5fabcabbc6d50def3a027086ee27
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-29 16:15:29 -0400

    module

--- 05.modules/ec2/ec2.tf
+++ 05.modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   count                     = 2
   ami                       = "ami-074df373d6bafa625"
-  instance_type             = "var.TYPE"
+  instance_type             = var.TYPE
   vpc_security_group_ids    = [var.SG_ID]
 
   tags                      = {
