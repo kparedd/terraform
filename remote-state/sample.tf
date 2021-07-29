2ef121a8b04e56d6324a499755e99925c5c393d5
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-29 11:57:27 -0400

    bucket s3 with dynamodb

--- remote-state/sample.tf
+++ remote-state/sample.tf
@@ -15,6 +15,6 @@ terraform {
     bucket                  = "terraform-kp58"
     key                      = "sample/terraform.tfstate"
     region                   = "us-east-1"
-    dynamodb_table           = terraform
+    dynamodb_table           = "terraform"
   }
 }
