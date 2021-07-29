18e498ac2a50e64f344ddb6c8fcd2f7a44208665
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-29 16:05:29 -0400

    module

--- 05.modules/ec2/ec2.tf
+++ 05.modules/ec2/ec2.tf
@@ -1,10 +1,17 @@
 resource "aws_instance" "sample" {
   ami                       = "ami-074df373d6bafa625"
-  instance_type             = "t3.micro"
+  instance_type             = "var.TYPE"
   vpc_security_group_ids    = [var.SG_ID]
+
   tags                      = {
     Name                    = "sample"
   }
 }
 
 variable "SG_ID" {}
+variable "TYPE" {}
+
+output "PRIVATE_IP" {
+  value = aws_instance.sample.private_ip
+
+}
--- 05.modules/main.tf
+++ 05.modules/main.tf
@@ -6,6 +6,10 @@ module "ec2" {
   depends_on = [module.sg]
   source = "./ec2"
   SG_ID  = module.sg.SG_ID
+  TYPE = "t3.small"
+}
+output "PRIVATE_IP" {
+  value = module.ec2.PRIVATE_IP
 }
 
 provider "aws" {
