fea4e17d310d023b0655ad08e26e4291d96f7349
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-30 13:15:55 -0400

    terraform

--- roboshop-shell-scripting/provider.tf
+++ roboshop-shell-scripting/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket            = "terraform-b57"
+    bucket            = "terraform-kp58"
     key               = "roboshop/shell-scripting/terraform.tfstate"
     region            = "us-east-1"
     dynamodb_table    = "terraform"
