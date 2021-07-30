18b7b0cbfa91eb79be898a472a2533ead2735b60
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-30 12:50:29 -0400

    terraform

---
+++ roboshop-shell scirpting/ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  count                   = length(var.COMPONENTS)
+  ami                     = "ami-074df373d6bafa625"
+  spot_price              = "0.0031"
+  instance_type           = "t3.micro"
+  wait_for_fulfillment    = true
+
+  tags = {
+    Name                  = element(var.COMPONENTS, count.index )
+  }
+}
+  resource "aws_ec2_tag" "name-tag" {
+    count                 = length(var.COMPONENTS)
+    resource_id           = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index )
+    key                   = "Name"
+    value                 = element(var.COMPONENTS, count.index )
+  }
