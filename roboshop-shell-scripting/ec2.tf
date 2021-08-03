d39b6265f5979c7c81976c28d842e3b82ffc7068
kparedd <83795719+kparedd@users.noreply.github.com>
2021-08-03 11:28:34 -0400

    terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_spot_instance_request" "cheap_worker"  {
   count                   = length(var.COMPONENTS)
   ami                     = "ami-074df373d6bafa625"
   spot_price              = "0.0031"
